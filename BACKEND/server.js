5ff8a236ded746fbd233ce9da986551fed210630
Kavishka Sulochana <kavishkaigalagama@gmail.com>
2024-10-02 12:33:10 +0530

    db_setup

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const app = express();
+require('dotenv').config();
 
 
 const PORT = process.env.PORT || 8070;
@@ -13,17 +14,16 @@ app.use(bodyParser.json());
 
 const URL = process.env.MONGODB_URL;
 
-mongoose.connect(URL, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-});
+mongoose.connect(URL, {});
 
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB connection success!");
 });
+
+const studentRouter = require('./routes/students.js');
+
+app.use('/student', studentRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is up and running on port number: ${PORT}`);
